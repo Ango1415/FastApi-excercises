569cf828775416353bd966b15cf4aca60e1361b8
Angel Gomez1 <angel_gomez1@epam.com>
2025-12-11 10:42:19 -0500

    4_body_multiple_parameters | body and query params

--- 4_body_multiple_parameters/main.py
+++ 4_body_multiple_parameters/main.py
@@ -12,25 +12,30 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
 
-"""
-This example demonstrates how to use FastAPI to define path, query and body parameters.
-First, of course, you can mix Path, Query and request body parameter declarations freely and FastAPI will know 
-what to do.
-Expected body example:
-{
-    "name": "Foo",
-    "description": "The pretender",
-    "price": 42.0,
-    "tax": 3.2
-}
-"""
+
+
 @app.put("/items/{item_id}")
 async def update_item(
     item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
     q: str | None = None,
     item: Item | None = None,
 ):
+    """
+    This example demonstrates how to use FastAPI to define path, query and body parameters.
+    First, of course, you can mix Path, Query and request body parameter declarations freely and FastAPI will know
+    what to do.
+    Expected body example:
+    {
+        "name": "Foo",
+        "description": "The pretender",
+        "price": 42.0,
+        "tax": 3.2
+    }
+    """
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
@@ -39,55 +44,78 @@ async def update_item(
     return results
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
 
-"""
-You can also declare multiple body parameters, e.g. item and user.
-In this case, FastAPI will notice that there is more than one body parameter in the function (there are two parameters 
-that are Pydantic models).
-So, it will then use the parameter names as keys (field names) in the body, and expect a body like:
-{
-    "item": {
-        "name": "Foo",
-        "description": "The pretender",
-        "price": 42.0,
-        "tax": 3.2
-    },
-    "user": {
-        "username": "dave",
-        "full_name": "Dave Grohl"
-    }
-}
-"""
 @app.put("/items/{item_id}/multiple_body/")
 async def update_item_multiple_body(item_id: int, item: Item, user: User):
+    """
+    You can also declare multiple body parameters, e.g. item and user.
+    In this case, FastAPI will notice that there is more than one body parameter in the function (there are two parameters
+    that are Pydantic models).
+    So, it will then use the parameter names as keys (field names) in the body, and expect a body like:
+    {
+        "item": {
+            "name": "Foo",
+            "description": "The pretender",
+            "price": 42.0,
+            "tax": 3.2
+        },
+        "user": {
+            "username": "dave",
+            "full_name": "Dave Grohl"
+        }
+    }
+    """
     results = {"item_id": item_id, "item": item, "user": user}
     return results
 
-"""
-You could decide that you want to have another key importance in the same body, besides the item and user.
-If you declare it as is, because it is a singular value, FastAPI will assume that it is a query parameter.
-But you can instruct FastAPI to treat it as another body key using Body
-Expected body example:
-{
-    "item": {
-        "name": "Foo",
-        "description": "The pretender",
-        "price": 42.0,
-        "tax": 3.2
-    },
-    "user": {
-        "username": "dave",
-        "full_name": "Dave Grohl"
-    },
-    "importance": 5
-}
-"""
+
 @app.put("/items/{item_id}/single_body_param/")
 async def update_item_single_body_param(
     item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
 ):
+    """
+    You could decide that you want to have another key importance in the same body, besides the item and user.
+    If you declare it as is, because it is a singular value, FastAPI will assume that it is a query parameter.
+    But you can instruct FastAPI to treat it as another body key using Body
+    Expected body example:
+    {
+        "item": {
+            "name": "Foo",
+            "description": "The pretender",
+            "price": 42.0,
+            "tax": 3.2
+        },
+        "user": {
+            "username": "dave",
+            "full_name": "Dave Grohl"
+        },
+        "importance": 5
+    }
+    """
     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+
+@app.put("/items/{item_id}/body_and_query/")
+async def update_item_body_and_query(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: Annotated[int, Body(gt=0)],
+    q: str | None = None,
+):
+    """
+    Of course, you can also declare additional query parameters whenever you need, additional to any body parameters.
+    As, by default, singular values are interpreted as query parameters, you don't have to explicitly add a Query.
+
+    * (Positional-only Arguments Separator): When used '*' alone, before other named parameters, it signifies that all
+    parameters following it must be passed as keyword arguments, not positional arguments [2]. This helps enforce clarity
+    and prevent errors in argument passing.
+    Example: def my_function(a, b, *, c, d): ---> my_function(1, 2, c=3, d=4)  # Works
+    """
+
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
     return results

546042fd5c4407f8aff0e45bd37e80d44c65abe8
Angel Gomez1 <angel_gomez1@epam.com>
2025-12-11 09:44:47 -0500

    2_path_parameters_and_numeric_validations

---
+++ 2_path_parameters_and_numeric_validations/main.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+"""
+This example demonstrates how to use FastAPI to define path parameters with numeric validations.
+The `item_id` path parameter is required and must be an integer between 1 and 1000 (inclusive).
+"""
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],    # Path param
+    q: str,                                                                             # Query param
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results

da064245f39d077ef10feb975f68867549ec4a4b
Angel Gomez1 <angel_gomez1@epam.com>
2025-12-11 11:51:50 -0500

    6_body_nested_models | Deeply nested models and bodies of pures lists

--- 6_body_nested_models/main.py
+++ 6_body_nested_models/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
 class Image(BaseModel):
-    url: str
+    """
+    You can use more complex singular types that inherit from str. To see all the options you have, checkout Pydantic's
+    Type Overview. You will see some examples in the next chapter. For example, as in the Image model we have a url
+    field, we can declare it to be an instance of Pydantic's HttpUrl instead of a str.
+    The string will be checked to be a valid URL, and documented in JSON Schema / OpenAPI as such.
+    """
+    url: HttpUrl
     name: str
 
 
@@ -20,7 +26,8 @@ class ItemNested(BaseModel):
     price: float
     tax: float | None = None
     tags: set[str] = set()
-    image: Image | None = None
+    #image: Image | None = None
+    images: list[Image] | None = None
 
 
 class Item(BaseModel):
@@ -34,6 +41,16 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
     tags: set[str] = set()
+
+
+class Offer(BaseModel):
+    """
+    You can define arbitrarily deeply nested models:
+    """
+    name: str
+    description: str | None = None
+    price: float
+    items: list[ItemNested]
 
 
 @app.put("/items/{item_id}")
@@ -50,13 +67,42 @@ async def update_item_nested(item_id: int, item: ItemNested):
         "description": "The pretender",
         "price": 42.0,
         "tax": 3.2,
-        "tags": ["rock", "metal", "bar"],
-        "image": {
-            "url": "http://example.com/baz.jpg",
-            "name": "The Foo live"
-        }
+        "tags": [
+            "rock",
+            "metal",
+            "bar"
+        ],
+        "images": [
+            {
+                "url": "http://example.com/baz.jpg",
+                "name": "The Foo live"
+            },
+            {
+                "url": "http://example.com/dave.jpg",
+                "name": "The Baz"
+            }
+        ]
     }
     Note that the image attribute is itself an object with a specific structure.
     """
     results = {"item_id": item_id, "item": item}
     return results
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    """
+    Notice how Offer has a list of Items, which in turn have an optional list of Images
+    """
+    return offer
+
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    """
+    If the top level value of the JSON body you expect is a JSON array (a Python list), you can declare the type in the parameter of the function
+    :param images:
+    :return: None
+    """
+    return images
+
